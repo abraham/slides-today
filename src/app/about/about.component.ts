fbaf29bbc71b3f25aafb11df742a354e42a95118
Abraham Williams <4braham@gmail.com>
2017-08-17 16:50:34 -0500

    Add ripples to about card

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements AfterViewInit {
 
   constructor() { }
 
+  @ViewChild('actions') actions;
+
   imageUrl = '/assets/img/about-medium.jpg';
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.initRipples();
+  }
+
+  initRipples(): void {
+    const buttons = this.actions.nativeElement.querySelectorAll('.mdc-button:not(.mdc-ripple-upgraded)');
+    Array.from(buttons).forEach(button => {
+      MDCRipple.attachTo(button);
+    });
   }
 
   openTwitter(handle: string): void {
@@ -21,5 +32,4 @@ export class AboutComponent implements OnInit {
   open(url: string): void {
     window.open(url);
   }
-
 }
