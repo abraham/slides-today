6b9d6b1e84484dae2cf3bbe15e9c842519402dd8
Abraham Williams <4braham@gmail.com>
2017-08-18 09:42:30 -0500

    Sorting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,30 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { NgModule } from '@angular/core';
 
-import { LazyLoadImageModule } from 'ng-lazyload-image';
-
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CardActionComponent } from './card-action/card-action.component';
 import { DeckComponent } from './deck/deck.component';
 import { DecksComponent } from './decks/decks.component';
 import { HeaderComponent } from './header/header.component';
-import { TagsComponent } from './tags/tags.component';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AboutComponent } from './about/about.component';
 import { InviteComponent } from './invite/invite.component';
-import { CardActionComponent } from './card-action/card-action.component';
 import { TagComponent } from './tag/tag.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
+    AboutComponent,
     AppComponent,
+    CardActionComponent,
     DeckComponent,
     DecksComponent,
     HeaderComponent,
-    TagsComponent,
-    AboutComponent,
     InviteComponent,
-    CardActionComponent,
     TagComponent,
+    TagsComponent,
   ],
   imports: [
     AppRoutingModule,
