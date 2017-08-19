978c87159b3e0e4f1d770f250d08fecee370429b
Abraham Williams <4braham@gmail.com>
2017-08-19 12:32:20 -0500

    added share fab to deck details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { InviteComponent } from './invite/invite.component';
 import { TagComponent } from './tag/tag.component';
 import { TagsComponent } from './tags/tags.component';
+import { ShareComponent } from './share/share.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { TagsComponent } from './tags/tags.component';
     InviteComponent,
     TagComponent,
     TagsComponent,
+    ShareComponent,
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/share/share.component.ts
@@ -0,0 +1,56 @@
+import { Component, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
+import { MDCSimpleMenu } from '@material/menu/dist/mdc.menu';
+
+@Component({
+  selector: 'app-share',
+  templateUrl: './share.component.html',
+  styleUrls: ['./share.component.css']
+})
+export class ShareComponent implements AfterViewInit {
+
+  constructor() { }
+
+  _menu: MDCSimpleMenu;
+  @ViewChild('fabEl') fabEl;
+  @ViewChild('menuEl') menuEl;
+  @Input() text: string;
+  services = {
+    google: () => `https://plus.google.com/share?url=${this.shareUrl()}`,
+    twitter: () => `https://twitter.com/intent/tweet?text=${this.shareText() + ' ' + this.shareUrl()}`,
+    facebook: () => `https://www.facebook.com/sharer/sharer.php?u=${this.shareUrl()}`,
+  };
+
+  ngAfterViewInit() {
+    this._menu = new MDCSimpleMenu(this.menuEl.nativeElement);
+    this.initRipples();
+  }
+
+  initRipples(): void {
+    MDCRipple.attachTo(this.fabEl.nativeElement);
+    // const items = this.menuEl.nativeElement.querySelectorAll('.mdc-list-item:not(.mdc-ripple-upgraded)');
+    // Array.from(items).forEach(item => {
+    //   MDCRipple.attachTo(item);
+    // });
+  }
+
+  shareText(): string {
+    return encodeURIComponent(this.text);
+  }
+
+  shareUrl(): string {
+    return encodeURIComponent(window.location.href);
+  }
+
+  toggleMenu(event) {
+    this._menu.open = !this._menu.open;
+  }
+
+  open(url) {
+    window.open(url);
+  }
+
+  share(service: string) {
+    window.open(this.services[service]());
+  }
+}
