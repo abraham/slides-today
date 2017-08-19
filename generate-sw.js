1ac9661f8f3e202dff4317140888fbf8f6afbc7e
Abraham Williams <4braham@gmail.com>
2017-08-19 17:29:37 -0500

    Initial pass at offline support

---
+++ generate-sw.js
@@ -0,0 +1,49 @@
+const workboxBuild = require('workbox-build');
+const SRC_DIR = 'src';
+const BUILD_DIR = 'dist';
+
+const input = {
+  swDest: `${BUILD_DIR}/sw.js`,
+  globDirectory: BUILD_DIR,
+  globPatterns: [
+    '**/*.{js,png,jpg,html,css}',
+    'assets/**/*'
+  ],
+  globIgnores: [
+    'sw.js',
+    '**/*original*'
+  ],
+  navigateFallback: '/index.html',
+  clientsClaim: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/maps\.googleapis\.com\/(.*)/,
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'runtime-maps',
+        cacheExpiration: {
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+        cacheableResponse: {statuses: [0, 200]}
+      }
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/(.*)/,
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'runtime-fonts',
+        cacheExpiration: {
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+        cacheableResponse: {statuses: [0, 200]}
+      }
+    }
+  ],
+  handleFetch: true,
+  maximumFileSizeToCacheInBytes: 4000000
+};
+
+workboxBuild.generateSW(input).then(() => {
+  console.log('The production service worker has been injected with a precache list.');
+});
