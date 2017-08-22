ad631104c041489fb354d08f4ccfa8b99e595ae9
Abraham Williams <4braham@gmail.com>
2017-08-21 22:28:44 -0500

    Add Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import 'autotrack/lib/plugins/event-tracker';
+import 'autotrack/lib/plugins/outbound-link-tracker';
+import 'autotrack/lib/plugins/page-visibility-tracker';
+import 'autotrack/lib/plugins/url-change-tracker';
+
 if (environment.production) {
   enableProdMode();
 }
