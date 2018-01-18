b52b652f1d33e1ea20c574b24cb0e4d572d5ba8e
Abraham Williams <4braham@gmail.com>
2018-01-17 21:21:48 -0600

    Add script to get tweets

---
+++ scripts/get-tweet.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const Twitter = require('twitter');
+
+const config = require(path.resolve('./.twitter.json'));
+const dataFilePath = path.resolve('./src/app/tweets.data.json');
+const allTweets = require(dataFilePath);
+
+const id = process.argv[2];
+const client = new Twitter({
+  consumer_key: config.consumerKey,
+  consumer_secret: config.consumerSecret,
+  access_token_key: config.accesTokenKey,
+  access_token_secret: config.accesTokenSecret
+});
+
+console.log(`Gettint Tweet ${id}`);
+
+client.get('statuses/show', { id: id, tweet_mode: 'extended', include_entities: true }, (error, tweet, response) => {
+  if(error) { console.log('ERROR', error); }
+  allTweets[id] = tweet;
+  fs.writeFileSync(dataFilePath, JSON.stringify(allTweets, null, 2));
+});
