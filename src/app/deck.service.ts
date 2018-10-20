29072602198640af97ed13a700e371c6b3d64a22
Abraham Williams <4braham@gmail.com>
2018-10-20 10:26:19 -0500

    Move deck data to json file

--- src/app/deck.service.ts
+++ src/app/deck.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Deck } from './deck';
-import { DECKS } from './decks.data';
+import DECKS from './decks.data.json';
+import { Link } from './link';
+
+// Get an ID with firebase.database().ref('test').push().key
+
+export const decks: Deck[] = DECKS.map(deck => {
+  deck.links = deck.links.map(link => new Link(link));
+  return new Deck(deck);
+});
 
 @Injectable()
 export class DeckService {
   getDecks(): Promise<Deck[]> {
-    return Promise.resolve(DECKS);
+    return Promise.resolve(decks);
   }
 
   getDeck(id: string): Promise<Deck> {
-    return Promise.resolve(DECKS.find(deck => deck.id === id));
+    return Promise.resolve(decks.find(deck => deck.id === id));
   }
 }
