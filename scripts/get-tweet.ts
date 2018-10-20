e9212e0030eb88e605f626f13252237c9b822f34
Abraham Williams <4braham@gmail.com>
2018-10-20 09:50:47 -0500

    Remove TS as

--- scripts/get-tweet.ts
+++ scripts/get-tweet.ts
@@ -6,12 +6,13 @@ import allTweets from '../src/app/tweets.data.json';
 
 const dataFilePath = path.resolve('./src/app/tweets.data.json');
 const id = process.argv[2];
-const client = new Twit({ ...twitterCredentials, strictSSL: true } as Twit.Options);
+const client = new Twit({ ...twitterCredentials, strictSSL: true });
+const showParams = { id: id, tweet_mode: 'extended', include_entities: true };
 
 if (id) {
   console.log(`Getting tweet ${id}`);
 
-  client.get('statuses/show', { id: id, tweet_mode: 'extended', include_entities: true }, (error, tweet, _response) => {
+  client.get('statuses/show', showParams, (error, tweet, _response) => {
     if (error) { console.log('ERROR', error); }
     (allTweets as { [index: string]: any })[id] = tweet;
     fs.writeFileSync(dataFilePath, JSON.stringify(allTweets, null, 2));
