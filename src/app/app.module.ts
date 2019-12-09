aa0e7987eaf5d590305b95b279cd3b33044b0e6d
Abraham Williams <4braham@gmail.com>
2019-12-08 21:48:31 -0600

    Add loading skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { UpdateComponent } from './update/update.component';
     BrowserModule,
     CardModule,
     LazyLoadImageModule,
+    NgxSkeletonLoaderModule,
     TagModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
