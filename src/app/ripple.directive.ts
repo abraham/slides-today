08f004d68e0c97746de31a1d733e3d3e4d488da6
Abraham Williams <4braham@gmail.com>
2020-03-09 21:18:25 -0500

    Only initialize ripple on mouseenter

--- src/app/ripple.directive.ts
+++ src/app/ripple.directive.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 import { MDCRipple } from '@material/ripple';
 
 type Style = 'unbounded' | 'bounded';
@@ -6,13 +6,22 @@ type Style = 'unbounded' | 'bounded';
 @Directive({
   selector: '[appRipple]'
 })
-export class RippleDirective implements AfterViewInit {
+export class RippleDirective {
+  private attached = false;
+
   @Input('appRipple') style?: Style;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.attach();
+  }
 
   constructor(private el: ElementRef) {}
 
-  ngAfterViewInit() {
-    const ripple = MDCRipple.attachTo(this.el.nativeElement);
-    ripple.unbounded = this.style !== 'bounded';
+  private attach() {
+    if (!this.attached) {
+      this.attached = true;
+      const ripple = MDCRipple.attachTo(this.el.nativeElement);
+      ripple.unbounded = this.style !== 'bounded';
+    }
   }
 }
