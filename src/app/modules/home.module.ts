e5b87037e28f2e4677c3356e9e9beb3a514218aa
Abraham Williams <4braham@gmail.com>
2020-03-09 22:23:13 -0500

    Implement a virtual scroller

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { RouterModule, Routes } from '@angular/router';
+import { VirtualScrollerModule } from 'ngx-virtual-scroller';
 import { AboutComponent } from '../about/about.component';
 import { DeckListComponent } from '../deck-list/deck-list.component';
 import { DeckSummaryComponent } from '../deck-summary/deck-summary.component';
@@ -31,6 +32,7 @@ const routes: Routes = [
     CommonModule,
     MatBottomSheetModule,
     TagModule,
+    VirtualScrollerModule,
     RouterModule.forChild(routes),
   ],
 })
