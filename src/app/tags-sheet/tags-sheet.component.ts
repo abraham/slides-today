c4daa1d83269319cadcab36dc69b655c59f8cc8f
Abraham Williams <4braham@gmail.com>
2020-03-22 13:42:53 -0500

    Switch components to display: block

--- src/app/tags-sheet/tags-sheet.component.ts
+++ src/app/tags-sheet/tags-sheet.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tags-sheet',
-  styleUrls: ['./tags-sheet.component.css'],
+  styleUrls: ['./tags-sheet.component.scss'],
   templateUrl: './tags-sheet.component.html',
 })
 export class TagsSheetComponent {
