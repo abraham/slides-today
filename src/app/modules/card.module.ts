1b9e5c9a03dba434a37edb5e3fa8768e75c73fb5
Abraham Williams <4braham@gmail.com>
2025-12-07 02:06:55 -0600

    Switch to MatRippleModule

--- src/app/modules/card.module.ts
+++ src/app/modules/card.module.ts
@@ -2,17 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CardComponent } from '../card/card.component';
-import { RippleDirective } from '../ripple.directive';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  exports: [CardComponent, RippleDirective],
+  exports: [CardComponent],
   imports: [
     CommonModule,
     MatButtonModule,
     RouterModule.forChild([]),
     CardComponent,
-    RippleDirective,
   ],
 })
 export class CardModule {}
--- src/app/ripple.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RippleDirective } from './ripple.directive';
-
-describe('RippleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RippleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
